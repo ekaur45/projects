b93e74cadcd3071eb976e8ce8982ec1b3ad7b5e5
ekaur45 <ekaur45@EKAUR45>
2018-02-16 01:38:48 +0500

    Add project files.

---
+++ GOGLIKE/Scripts/site.js
@@ -0,0 +1,16 @@
+﻿var removedPrice = 0;
+function item_deal_hover(id) {
+    removedPrice = "";
+    $("#"+id).addClass("btn-success");
+    $("#"+id).removeClass("info");
+    removedPrice = $("#"+id).children()[0].innerHTML;
+    $("#"+id).children()[0].innerHTML = "BUY NOW"
+
+    $("#"+id).children()[0].classList.remove("item-offer");
+}
+function item_deal_mouseleave(id) {
+    $("#"+id).addClass("info");
+    $("#"+id).removeClass("btn-success");
+    $("#"+id).children()[0].innerHTML = "PKR 1990";
+    $("#"+id).children()[0].classList.add("item-offer");
+}
