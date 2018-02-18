8312e40bee8f05ae91dd2d0c89d44ebb9777db1e
ekaur45 <ekaur45@EKAUR45>
2018-02-18 22:35:56 +0500

    commit

--- GOGLIKE/Scripts/site.js
+++ GOGLIKE/Scripts/site.js
@@ -1,16 +1,36 @@
-﻿var removedPrice = 0;
-function item_deal_hover(id) {
-    removedPrice = "";
-    $("#"+id).addClass("btn-success");
-    $("#"+id).removeClass("info");
-    removedPrice = $("#"+id).children()[0].innerHTML;
-    $("#"+id).children()[0].innerHTML = "BUY NOW"
-
-    $("#"+id).children()[0].classList.remove("item-offer");
+﻿var x = 1;
+function slider() {
+    if (x >= 10)
+        x = 1;
+    //$("#slider").fadeOut(800);
+    
+    $('#slider').fadeTo(900, function () {
+        $(this).css({ "background-image": "url('games/" + x + ".jpg')" }).fadeIn(900);
+    });
+    x++;
+    setTimeout(function () {
+        slider();
+    }, 8000);
 }
-function item_deal_mouseleave(id) {
-    $("#"+id).addClass("info");
-    $("#"+id).removeClass("btn-success");
-    $("#"+id).children()[0].innerHTML = "PKR 1990";
-    $("#"+id).children()[0].classList.add("item-offer");
+$(document).ready(function () {
+    slider();
+    change('', 'div1');
+    $('.browse-mwnu-item')[1].classList.add('banner');
+    $('.half-menu>.browse-mwnu-item')[1].classList.add('banner');
+});
+function change(this_, div) {
+    if (this_ != '') {
+        for (var i = 1; i < 5; i++) {
+            $('.div' + i).css('display', "none");
+        }
+        $('.browse-mwnu-item').removeClass('banner')
+        $('.' + div).css('display', "block");
+        this_.classList.add('banner');
+    } else {
+        for (var i = 1; i < 5; i++) {
+            $('.div' + i).css('display', "none");
+        }
+        $('.browse-mwnu-item').removeClass('banner')
+        $('.' + div).css('display', "block");
+    }
 }
